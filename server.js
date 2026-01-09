c0312f03252b35880b0e0798f297f5fa84c1c1d4
darkosBE <emzlibres06@gmail.com>
2026-01-09 20:29:42 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8081;
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// For all other requests, serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Frontend server listening at http://localhost:${port}`);
+  console.log(`Credits to syzdark & Herick For making this project`);
+});
