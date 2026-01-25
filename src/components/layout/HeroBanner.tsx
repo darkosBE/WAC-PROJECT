0e74dc09a7157c510b4ed55846f5c78a80a6b72b
YUBS (darkosBE) <emzlibres06@gmail.com>
2026-01-25 12:46:04 +0800

    Add files via upload

--- src/components/layout/HeroBanner.tsx
+++ src/components/layout/HeroBanner.tsx
@@ -33,11 +33,7 @@ export function HeroBanner() {
                         <Sparkles className="w-5 h-5 mr-2" />
                         Get Started
                     </Button>
-                    <img
-                        src="/character-illustration.png"
-                        alt="Gaming Setup"
-                        className="w-32 h-32 object-contain hidden lg:block"
-                    />
+
                 </div>
             </div>
         </div>
